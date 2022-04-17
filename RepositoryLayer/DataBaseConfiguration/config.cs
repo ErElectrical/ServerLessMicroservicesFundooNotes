e9f86d589a2374d1c2e4dce2bd16738bb9c7f580
ErElectrical <mohittanwar786.mt@gmail.com>
2022-04-17 11:29:05 +0100

    Mohit added : CosmosDb configurations

---
+++ RepositoryLayer/DataBaseConfiguration/config.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.DataBaseConfiguration
+{
+    public class config
+    {
+        private readonly IConfiguration _toolsetting;
+
+        private Database database;
+
+        private Container container;
+
+        private CosmosClient cosmosClient;
+
+
+
+
+        public config(IConfiguration _toolsetting)
+        {
+            this._toolsetting = _toolsetting;
+        }
+
+        public  async  void Appconfiguration()
+        {
+            var databaseName = this._toolsetting["Values:DataBaseId"];
+            var containerName = this._toolsetting["Values:ContainerId"];
+            //CosmosClient client = new DocumentClient(new Uri(this._toolsetting["Values:DocDbEndpoint"]), this._toolsetting["Values:DocDbMasterKey"]);
+
+            this.database = await this.cosmosClient.CreateDatabaseIfNotExistsAsync(databaseName);
+            this.container = await this.database.CreateContainerIfNotExistsAsync(containerName,"/Id");
+            
+        }
+    }
+}
---
+++ RepositoryLayer/DataBaseConfiguration/startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Azure.Cosmos.Fluent;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RepositoryLayer.DataBaseConfiguration;
+
+[assembly: FunctionsStartup(typeof(UserService.Startup))]
+
+namespace UserService
+{
+    public class Startup : FunctionsStartup
+    {
+        private static readonly IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // Register the CosmosClient as a Singleton
+
+            builder.Services.AddSingleton((s) => {
+
+                var connectionString = configuration["CosmosDBConnection"];
+                if (string.IsNullOrEmpty(connectionString))
+                {
+                    throw new ArgumentNullException("Please specify a valid connection string in the local.settings.json file or your Azure Functions Settings.");
+                }
+                if(connectionString != null)
+                {
+                    config con = new config(configuration);
+                    con.Appconfiguration();
+                }
+
+                CosmosClientBuilder configurationBuilder = new CosmosClientBuilder(connectionString);
+                return configurationBuilder
+                        .Build();
+            });
+        }
+
+
+    }
+
+
+}
